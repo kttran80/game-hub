b00e773665ddc8721211b0a1788b85f8e741433c
kttran80 <tranthekiet@yahoo.com>
2023-12-13 13:29:11 -0600

    fixing building errors

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
